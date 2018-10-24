c8839b68c06433b39bef90f3285e997ea6c9fa16
guozetang <guozetang@gmail.com>
2018-10-24 12:50:33 -0400

    Convert test

--- userscripts/build.sh
+++ userscripts/build.sh
@@ -13,5 +13,5 @@ git config --global user.name "guozetang" #填入GitHub的用户名
 # git branch -mv origin master
 git add --all .
 git commit -m "Travis CI Auto Builder" #自动构建后的内容将全部以此信息提交
-git push -u --quiet --force https://${BLOG_REPO_TOKEN}@github.com/guozetang/guozetang.github.io.git master  #自动构建后的内容将全部以此信息提交
+git push --quiet --force https://${REPO_TAKEN}@github.com/guozetang/guozetang.github.io.git master  #自动构建后的内容将全部以此信息提交
 #curl --connect-timeout 20 --max-time 30 -s http://远端服务器URL/webhook.php?_=${NOTIFY_TOKEN} #服务器Webhook 将在下文介绍
