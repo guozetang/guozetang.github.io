1eb8b667668cf3dd53057af8669299e57f99e0fe
guozetang <guozetang@gmail.com>
2018-10-29 00:19:46 -0400

    Add leetcode 121

--- source/_drafts/test.cpp
+++ source/_drafts/test.cpp
@@ -1,24 +1,19 @@
 class Solution {
- public:
-  int scoreOfParentheses(string S) {
-    int res = 0;
-    std::stack<char> par_stack;
-    bool flag = false;
-    for (int i = 0; i < S.size(); ++i) {
-      if (S[i] == '(') {
-        par_stack.push(S[i]);
-        flag = true;
-      } else if (flag == true) {
-        flag = false;
-        if (par_stack.size() == 1)
-          res++;
-        else
-          res += 1 * pow(2, par_stack.size() - 1);
-        par_stack.pop();
-      } else
-        par_stack.pop();
+public:
+    int maxProfit(vector<int>& prices) {
+      if(prices.empty()) return 0;
+      int preMin = prices[0], curMax = 0, maxPrice=0;
+      for(int i = 0; i < prices.size(); ++i) {
+        if (prices[i] > curMax){
+          curMax = prices[i];
+          maxPrice = max(maxPrice, curMax - preMin);
+        }
+        
+        if (prices[i] < preMin) {
+          curMax = 0;
+          preMin = prices[i];
+        }
+      }
+      return maxPrice;
     }
-
-    return res;
-  }
 };
